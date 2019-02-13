9c97201edc622cbdb38e01ecce5edf3c4aa60217
karltext <44974965+karltext@users.noreply.github.com>
2019-02-13 13:05:06 +0000

    structure

--- app/patients.py
+++ app/patients.py
@@ -6,7 +6,7 @@ from models import Patient
 # define your blueprint
 patient_bp = Blueprint('profiles', __name__, url_prefix='/patients')
 
-# create routs in the same way you would do @app.route
+# create routes in the same way you would do @app.route
 @patient_bp.route('/list')
 def view_patients():
     return 'ok'
