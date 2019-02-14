c402f4574b34401c19ef3101a3528c8ca3f7eb76
karltext <44974965+karltext@users.noreply.github.com>
2019-02-14 14:24:08 +0000

    sss

--- app/reports.py
+++ app/reports.py
@@ -12,28 +12,6 @@ from models import Report
 # define your blueprint
 report_bp = Blueprint('reports', __name__, url_prefix='/reports')
 
-
-@report_bp.route('/patient/<int:patient_id>')
-def view_patient_reports(patient_id):
-    ptreports = Report.query.filter_by(patient_id=patient_id)
-    return render_template('reports/patient.html', ptreports=ptreports)
-
-@report_bp.route('/lab-manager/<int:lm_id>')
-def view_lab_manager_reports(lm_id):
-    lmreports = Report.query.filter_by(lm_id=lm_id)
-    return render_template('viewreport.html', lmreports=lmreports)
-
-#/reports/register
-@report_bp.route('/register', methods=['POST', 'GET'])
-def register_report():
-    'Api end-point for creating a new user'
-    r = Report(**request.form)
-    db.session.add(r)
-    db.session.commit()
-    #return jsonify(r.to_dict())
-    return render_template('/reports/createreport.html', r=r)
-
-
 # /reports/create
 @report_bp.route('/create', methods=['POST', 'GET'])
 def create_report():
@@ -44,7 +22,6 @@ def create_report():
     # fetch the newly created for display of successful response
     r = db.session.query(Report).order_by(Report.report_id.desc()).first()
     return jsonify(r.to_dict())
-
 
 # /reports/view/<report_id>
 @report_bp.route('/view/<int:report_id>')
