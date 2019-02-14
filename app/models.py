c06c90de513d7a9d5702f5866fef50ea2ddddd9f
karltext <44974965+karltext@users.noreply.github.com>
2019-02-14 09:54:18 +0000

    morning

--- app/models.py
+++ app/models.py
@@ -13,25 +13,30 @@ from flask_sqlalchemy import declarative_base
 db = SQLAlchemy()
 Base = declarative_base()
 
+
 class Serializable(object):
     '''Helper class for converting SQLAlchemy objects to dicts
     for json serialisation.'''
+
     def to_dict(self):
-        return {c.name: getattr(self, c.name) 
-                for c in self.__table__.columns}
+        cols = self.__table__.columns
+        return {c.name: getattr(self, c.name) for c in cols}
 
 
 #### Models
 
-diagnosis_table = db.Table('diagosis',
-    db.Column('patient_id', 
-              db.Integer, 
-              db.ForeignKey('patient.patient_id'),
-              primary_key=True),
-    db.Column('problem_id', 
-              db.Integer, 
-              db.ForeignKey('problem.problem_id'),
-              primary_key=True))
+diagnosis_table = db.Table(
+    'diagosis',
+    db.Column(
+        'patient_id',
+        db.Integer,
+        db.ForeignKey('patient.patient_id'),
+        primary_key=True),
+    db.Column(
+        'problem_id',
+        db.Integer,
+        db.ForeignKey('problem.problem_id'),
+        primary_key=True))
 
 
 class LabManager(db.Model, Serializable):
@@ -50,9 +55,8 @@ class Patient(db.Model, Serializable):
     height = db.Column(db.Float)
     weight = db.Column(db.Float)
     location = db.Column(db.String(50))
-    problems = db.relationship('Problem', 
-                                secondary=diagnosis_table, 
-                                lazy='subquery')
+    problems = db.relationship(
+        'Problem', secondary=diagnosis_table, lazy='subquery')
 
 
 class Problem(db.Model, Serializable):
