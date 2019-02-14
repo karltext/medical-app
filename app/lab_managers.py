16a660543515794ffedd16c3434b47c67dc9d9d1
karltext <44974965+karltext@users.noreply.github.com>
2019-02-14 14:21:01 +0000

    patient stuff

--- app/lab_managers.py
+++ app/lab_managers.py
@@ -1,22 +1,35 @@
 from flask import Blueprint
 from flask import request
 from flask import jsonify
+from flask import render_template
 
 # import your required models
 from models import db
 from models import LabManager
+from models import Patient
+from models import Report
 
 # define your blueprint
 lab_manager_bp = Blueprint('lab_managers', __name__, url_prefix='/lab_managers')
 
+# /lab_managers/view/<lm_id>
+@lab_manager_bp.route('/view/<int:lm_id>')
+def view_lab_manager(lm_id):
+    'View a single lab_manager profile'
+    lab_manager = LabManager.query.filter_by(lm_id=lm_id).first()
+    return render_template('lab_managers/view.html', lab_manager=lab_manager)
 
+# /lab_managers/<int:lm_id>/patient/<int:patient_id>
+@lab_manager_bp.route('/<int:lm_id>/patient/<int:patient_id>')
+def view_patient_reports(lm_id, patient_id):
+    'view reports for a specfic patient'
+    context = dict(
+        lab_manager=LabManager.query.filter_by(lm_id=lm_id).first(),
+        reports=Report.query.filter_by(lm_id=lm_id, patient_id=patient_id),
+        patient=Patient.query.filter_by(patient_id=patient_id).first())
+    return render_template('lab_managers/patient.html', **context)
 
-
-# /lab_managers/private/list
-@lab_manager_bp.route('/list')
-def view_lab_manager():
-    lab_managers = db.session.query(LabManager).all()
-    return jsonify([p.to_dict() for p in lab_managers])
+#### Private helpers for creating and debugging.
 
 # /lab_managers/private/register
 @lab_manager_bp.route('/register', methods=['POST'])
@@ -25,3 +38,4 @@ def register_lab_manager():
     db.session.add(p)
     db.session.commit()
     return jsonify(p.to_dict())
+
